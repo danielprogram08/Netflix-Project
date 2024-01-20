319b7dcce427e74a118eba9864fa0e773ae6a626
danielprogram08 <danieldanielsilva08@gmail.com>
2024-01-20 13:00:33 -0300

    Inclusão do arquivo JS!

---
+++ index.js
@@ -0,0 +1,7 @@
+function cadastrar() {
+    const email = document.querySelector('input[name="email"]').value;
+    const senha = document.querySelector('input[name="senha"]').value;
+
+    console.log("Seu email: " + email);
+    console.log("Sua senha: " + senha);
+}
